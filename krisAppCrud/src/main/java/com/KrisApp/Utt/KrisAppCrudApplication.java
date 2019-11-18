84da9306ab1d50cb603b106047b5acca9012652f
dero <dero@dero>
2019-11-18 11:43:47 -0600

    Initial Commit, Workspace setup and Mysql. Close #1

---
+++ krisAppCrud/src/main/java/com/KrisApp/Utt/KrisAppCrudApplication.java
@@ -0,0 +1,13 @@
+package com.KrisApp.Utt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrisAppCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrisAppCrudApplication.class, args);
+	}
+
+}
---
+++ krisAppCrud/src/test/java/com/KrisApp/Utt/KrisAppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KrisApp.Utt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrisAppCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
